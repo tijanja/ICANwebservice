2d6c6b4588cb8debcaf107ad6946edeafdd15159
tijanja007 <adetunji.akinde@techadvance.ng>
2018-06-21 17:27:48 +0100

    commit 3

---
+++ controller/ICAN.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ICAN
+ *
+ * @author tunjiakinde
+ */
+class ICAN {
+    //put your code here
+}
